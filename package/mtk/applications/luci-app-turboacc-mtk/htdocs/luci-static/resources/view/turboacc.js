6301b1ca22016becbade92c94b4f2ea600c9acd9
Ashley Lee <nekokecore@emtips.net>
2025-02-01 13:07:13 +0000

    firewall:fix fullcone net

--- package/mtk/applications/luci-app-turboacc-mtk/htdocs/luci-static/resources/view/turboacc.js
+++ package/mtk/applications/luci-app-turboacc-mtk/htdocs/luci-static/resources/view/turboacc.js
@@ -249,15 +249,6 @@ return view.extend({
 			o.depends('fastpath_mh_eth_hnat', '1');
 		}
 
-		o = s.option(form.ListValue, 'fullcone', _('Full cone NAT'),
-			_('Full cone NAT (NAT1) can improve gaming performance effectively.'));
-		o.value('0', _('Disable'))
-		if (features.hasXTFULLCONENAT)
-			o.value('1', _('XT_FULLCONE_NAT'));
-		o.value('2', _('Boardcom_FULLCONE_NAT'));
-		o.default = '0';
-		o.rmempty = false;
-
 		o = s.option(form.ListValue, 'tcpcca', _('TCP CCA'),
 			_('TCP congestion control algorithm.'));
 		for (var i of features.hasTCPCCA.split(' ').sort())
